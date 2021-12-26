b3c527af159468aec44a0dcf2695cc5e5952b382
MrNiNaD <ninadparkar273@gmail.com>
2021-12-26 23:52:38 +0530

    feat: completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const http = require("http");
+const post = require("./server.json");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(post));
+});
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
